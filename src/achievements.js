37f5f866e115b7cb398f8f6bf69317befac1f5d5
rajatsharma1602 <rrajatssharma@gmail.com>
2017-07-09 15:49:25 +0530

    completed timeline design

--- src/achievements.js
+++ src/achievements.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './achievements.css'
+import Footer from './footer/footer.js'
+
 
 class Achievements extends React.Component {
   constructor(props) {
@@ -9,17 +12,119 @@ class Achievements extends React.Component {
   render() {
     return (
       // this is the segment to be loaded
+      <div>
         <div className="container">
-          <div className="row">
-            <div className="col-md-3 col-xs-12">
-              <span>Jan 2017</span>
+          <div className="timeline-item col-md-11 col-md-offset-1" >
+            <span className="date_time">20-07-1990</span>
+            <div className="row">
+
+              <div className="achievement-item col-md-6">
+                <div className="col-md-12 bdr">
+                  <div className="col-md-12">
+                    <div className="col-md-4 center-block">
+                      <img className="img-responsive img-circle" src="./media/1.jpg" alt="sample pic" />
+                    </div>
+                    <div className="col-md-8">
+                      <p><label>Rajat Sharma</label></p>
+                      <p><label>1<sup>st</sup></label></p>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <p>Im speaking with myself, number one, because I have a very good brain and Ive said a lot of things. I write the best placeholder text, and I'm the biggest developer on the web by far... While that's mock-ups and this is politics, are they really so different? I think the only card she has is the Lorem card.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="achievement-item col-md-6">
+                <div className="col-md-12 bdr">
+                  <div className="col-md-12">
+                    <div className="col-md-4 center-block">
+                      <img className="img-responsive img-circle" src="./media/1.jpg" alt="sample pic" />
+                    </div>
+                    <div className="col-md-8">
+                      <p><label>Rajat Sharma</label></p>
+                      <p><label>1<sup>st</sup></label></p>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <p>Im speaking with myself, number one, because I have a very good brain and Ive said a lot of things. I write the best placeholder text, and I'm the biggest developer on the web by far... While that's mock-ups and this is politics, are they really so different? I think the only card she has is the Lorem card.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="achievement-item col-md-6">
+                <div className="col-md-12 bdr">
+                  <div className="col-md-12">
+                    <div className="col-md-4 center-block">
+                      <img className="img-responsive img-circle" src="./media/1.jpg" alt="sample pic" />
+                    </div>
+                    <div className="col-md-8">
+                      <p><label>Rajat Sharma</label></p>
+                      <p><label>1<sup>st</sup></label></p>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <p>Im speaking with myself, number one, because I have a very good brain and Ive said a lot of things. I write the best placeholder text, and I'm the biggest developer on the web by far... While that's mock-ups and this is politics, are they really so different? I think the only card she has is the Lorem card.</p>
+                  </div>
+                </div>
+              </div>
+
             </div>
-            <div className="col-md-9 col-xs-12 col-xs-offset-2 text-justify">
-              <div className="well"><span>Look I am a well!</span></div>
+
+          </div>
+
+          <div className="timeline-item col-md-11 col-md-offset-1">
+            <span className="date_time">20-07-1990</span>
+            <div className="row">
+
+              <div className="achievement-item col-md-6">
+                <div className="col-md-12 bdr">
+                  <div className="col-md-12">
+                    <div className="col-md-4 center-block">
+                      <img className="img-responsive img-circle" src="./media/1.jpg" alt="sample pic" />
+                    </div>
+                    <div className="col-md-8">
+                      <p><label>Rajat Sharma</label></p>
+                      <p><label>1<sup>st</sup></label></p>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <p>Im speaking with myself, number one, because I have a very good brain and Ive said a lot of things. I write the best placeholder text, and I'm the biggest developer on the web by far... While that's mock-ups and this is politics, are they really so different? I think the only card she has is the Lorem card.</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className="timeline-item col-md-11 col-md-offset-1 ">
+            <span className="date_time">20-07-1990</span>
+            <div className="row">
+
+              <div className="achievement-item col-md-6">
+                <div className="col-md-12 bdr">
+                  <div className="col-md-12">
+                    <div className="col-md-4 center-block">
+                      <img className="img-responsive img-circle" src="./media/1.jpg" alt="sample pic" />
+                    </div>
+                    <div className="col-md-8">
+                      <p><label>Rajat Sharma</label></p>
+                      <p><label>1<sup>st</sup></label></p>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <p>Im speaking with myself, number one, because I have a very good brain and Ive said a lot of things. I write the best placeholder text, and I'm the biggest developer on the web by far... While that's mock-ups and this is politics, are they really so different? I think the only card she has is the Lorem card.</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="well well-sm">...</div>
+
         </div>
+
+        <Footer />
+      </div>
     );
   }
 }
--- src/footer/footer.js
+++ src/footer/footer.js
@@ -7,36 +7,37 @@ export default class Footer extends React.Component {
   render() {
     return(
       //footer html
-      <div className="container footer_container">
-
-        <div className="col-md-4 footer-social-links text-left">
-          <div>
-          <ul>
-            <li className=".hvr-push">
-              <a target="_blank" href="https://www.facebook.com/tssgndu"><span className="fa fa-facebook"></span></a>
-            </li>
-            <li className=".hvr-push">
-              <a target="_blank" href="https://www.twitter.com/gndutss"><span className="fa fa-twitter"></span></a>
-            </li>
-            <li className=".hvr-push">
-              <a target="_blank" href="https://www.linkedin.com/in/technicalstudentsociety"><span className="fa fa-linkedin"></span></a>
-            </li>
-            <li className=".hvr-push">
-              <a target="_blank" href="https://www.instagram.com/tss_gndu/"><span className="fa fa-instagram"></span></a>
-            </li>
-          </ul>
+      <div className=" footer_container">
+        <div className="container">
+          <div className="col-md-4 footer-social-links text-left">
+            <div>
+            <ul>
+              <li className=".hvr-push">
+                <a target="_blank" href="https://www.facebook.com/tssgndu"><span className="fa fa-facebook"></span></a>
+              </li>
+              <li className=".hvr-push">
+                <a target="_blank" href="https://www.twitter.com/gndutss"><span className="fa fa-twitter"></span></a>
+              </li>
+              <li className=".hvr-push">
+                <a target="_blank" href="https://www.linkedin.com/in/technicalstudentsociety"><span className="fa fa-linkedin"></span></a>
+              </li>
+              <li className=".hvr-push">
+                <a target="_blank" href="https://www.instagram.com/tss_gndu/"><span className="fa fa-instagram"></span></a>
+              </li>
+            </ul>
+            </div>
           </div>
-        </div>
 
-        <div className="col-md-4 footer-element">
-          <div className="footer-creadit">
-            <span className="theme_color">TSS</span> © 2016 All Right Reserved<br />
-            Designed by: <a target="_blank" href="http://codewarriors.tssgndu.com"><b>CODE WARRIORS</b></a>
+          <div className="col-md-4 col-sm-5 text-center">
+            <div className="footer-credit">
+              <span className="theme_color">TSS</span> © 2016 All Right Reserved<br />
+              Designed by: <a target="_blank" href="http://codewarriors.tssgndu.com"><b>CODE WARRIORS</b></a>
+            </div>
           </div>
-        </div>
 
-        <div className="col-md-4 text-right footer-element">
-          &nbsp;
+          <div className="col-md-4 text-right footer-element">
+            &nbsp;
+          </div>
         </div>
       </div>
     );
